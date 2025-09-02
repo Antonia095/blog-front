f51fd4f826192641849e5c0985daff250dac7eef
Antonia Maciel <antonia.josy95@gmail.com>
2025-09-02 08:34:32 -0300

    estrututacao do projeto

---
+++ src/pages/Home.tsx
@@ -0,0 +1,9 @@
+
+const Home = () => (
+  <div>
+    <h1>Bem-vindo ao meu blog!</h1>
+    <p>Este é o meu espaço para compartilhar ideias e projetos.</p>
+  </div>
+);
+
+export default Home;
---
+++ src/routes.tsx
@@ -0,0 +1,14 @@
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+
+const AppRoutes = () => (
+  <Router>
+    <Navbar />
+    <Routes>
+      <Route path="/" element={<Home />} /> 
+    </Routes>
+  </Router>
+);
+
+export default AppRoutes;
